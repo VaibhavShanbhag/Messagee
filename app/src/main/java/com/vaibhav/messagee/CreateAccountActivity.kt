94c49865bee52aa042bb2b10f50eb072d00a2697
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2022-05-28 16:48:00 +0530

    Added User Authentication

--- app/src/main/java/com/vaibhav/messagee/CreateAccountActivity.kt
+++ app/src/main/java/com/vaibhav/messagee/CreateAccountActivity.kt
@@ -1,14 +1,23 @@
 package com.vaibhav.messagee
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_create_account.*
 
 class CreateAccountActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var diaglog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        mAuth = FirebaseAuth.getInstance()
+        diaglog = ProgressDialog(this)
 
         tvloginaccount.setOnClickListener {
             Intent(this,LoginActivity::class.java).also {
@@ -16,5 +25,39 @@ class CreateAccountActivity : AppCompatActivity() {
             }
         }
 
+        createaccountbutton.setOnClickListener {
+            diaglog.setMessage("Creating New Account..")
+            diaglog.show()
+
+            val nameText: String = etname.text.toString()
+            val emailText: String = etemail.text.toString()
+            val passwordText: String = etpassword.text.toString()
+            val confirmPasswordText: String = etconfirmpass.text.toString()
+
+            if(passwordText.length < 8)
+                Toast.makeText(this,"Password length cannot be less than 8",Toast.LENGTH_SHORT).show()
+
+            else if(!(passwordText.equals(confirmPasswordText)))
+                Toast.makeText(this,"Confirm Password Doesn't match",Toast.LENGTH_SHORT).show()
+
+            else
+                signup(nameText,emailText,passwordText)
+        }
+
+    }
+
+    private fun signup(nameText: String, emailText: String, passwordText: String) {
+        mAuth.createUserWithEmailAndPassword(emailText, passwordText)
+            .addOnCompleteListener(this) { task ->
+                diaglog.dismiss()
+                if (task.isSuccessful) {
+                    Intent(this, LoginActivity::class.java).also {
+                        startActivity(it)
+                    }
+
+                } else {
+                    Toast.makeText(this,"Some Error occured",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/vaibhav/messagee/LoginActivity.kt
+++ app/src/main/java/com/vaibhav/messagee/LoginActivity.kt
@@ -1,14 +1,23 @@
 package com.vaibhav.messagee
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var diaglog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+        diaglog = ProgressDialog(this)
 
         tvforgotpass.setOnClickListener {
             Intent(this,ForgotPasswordActivity::class.java).also {
@@ -21,5 +30,28 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+
+        login_button.setOnClickListener {
+            diaglog.setMessage("Trying to login. Please wait..")
+            diaglog.show()
+
+            val email: String = etemail.text.toString()
+            val password: String = etpassword.text.toString()
+
+            login(email,password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                diaglog.dismiss()
+                if (task.isSuccessful) {
+                    Toast.makeText(this,"Logged In",Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Toast.makeText(this,"User Does Not Exist", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
